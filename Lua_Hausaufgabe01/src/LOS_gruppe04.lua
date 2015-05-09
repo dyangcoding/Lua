02f0a1e2b53e29dcf1169e6180ed40556667eb1b
fabian082 <fabian082yang@hotmail.com>
2015-05-09 18:41:31 +0200

    add Lua_Hausaufgaben 01

--- Lua_Hausaufgabe01/src/LOS_gruppe04.lua
+++ Lua_Hausaufgabe01/src/LOS_gruppe04.lua
@@ -1,30 +1,53 @@
---function Enum(t)
-  --local _name = t[1]
-  --local table = {}
-  
-  --local newClass = {
-    
-  
- -- }
-  
-   
---end
+local _classmt, _classvaluemt
+
+-- Metatable for classes
+_classmt = {
+
+  __index = function(t,k)
+  return t._attribut[k]
+  end,
+
+  __tostring = function( t )
+    return t._name
+  end
+
+}
+
+_classvaluemt = {
+
+  __tostring = function( t )
+    return t._value
+  end
+
+}
 
 function Class (t)
 
-  local o = {} -- local object 
   local name = t[1]
   
-  --local objecttable = {}
-  --setmetatable(o,table)
+  local o = {_name = name, _attribut={}} -- local object
+  
+  if(t[2] ~= nil) then
+  local attributes = t[2]
+   
+    for k,v in pairs[attributes] do
+    
+    local attributesvalue = {_name = o , _values = v}
+    setmetatable(attributesvalue, _classvaluemt)
+    o._attribut[v] = attributesvalue
+    
+    end
+  end
+  
+  
   _G[name] = o
-     
+  setmetatable(o, _classmt)   
   
   
   function o:create()
     local ob ={}
     setmetatable = {ob,self}
-    self._index = self
+    self._index = o
     return ob
   end
   
